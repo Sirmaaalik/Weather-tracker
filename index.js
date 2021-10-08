45b59472cb2d3c51bc1aaeb69ff240b11c9d572f
Sirmaaalik <malikrharris@gmail.com>
2021-10-07 21:00:07 -0400

    Added files

---
+++ index.js
@@ -0,0 +1,6 @@
+var search = document.createElement('div');
+var city = document.createElement('div');
+var forecast = document.createElement('div');
+
+// Search for city section
+var cityInput = document.createElement('input');
