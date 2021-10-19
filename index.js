8340aac807a6ceb43a3758dd7b638eb9f3b7aed7
Sirmaaalik <malikrharris@gmail.com>
2021-10-18 21:08:38 -0400

    Made another .then for the second api

--- index.js
+++ index.js
@@ -27,41 +27,40 @@ document.body.appendChild(search);
 // Weather info
 var city = document.createElement('div');
 var weather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=miami&appid=734bbaa3706a832f46be371dd62d57a4").then(
-        async placement => {
-            const place = await placement.json();
-            console.log(place);
-            let latitude = document.createElement('p');
-            let longitude = document.createElement('p');
-            latitude.innerHTML = place.coord.lat;
-            longitude.innerHTML = place.coord.lon;
-            console.log(latitude);
-            console.log(longitude);
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=miami&appid=734bbaa3706a832f46be371dd62d57a4")
+    .then(async placement => {
+        const place = await placement.json();
+        var latitude = place.coord.lat;
+        var longitude = place.coord.lon;
+        
+        return {latitude, longitude};            
+    })
+    .then(async ({latitude, longitude}) => {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=734bbaa3706a832f46be371dd62d57a4`)
+        .then(async response => {        
+            const data = await response.json();
+            console.log(data);
+
+            function card() {
+                let name = document.createElement('p');
+                let date = document.createElement('p');
+                let temperature = document.createElement('p');
+                let humidity = document.createElement('p');
+                let wind = document.createElement('p');
+                let indexUV = document.createElement('p');
+                name.innerHTML = data.name;
+                // date.innerHTML = data.date;
+                temperature.innerHTML = data.main.temp;
+                humidity.innerHTML = data.main.humidity;
+                wind.innerHTML = data.wind.speed;
+                indexUV.innerHTML = 
+
+                city.appendChild(name);
+                city.appendChild(temperature);
+                document.body.appendChild(city);            
+            };
+            card();
         });
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=734bbaa3706a832f46be371dd62d57a4`).then(
-        async response => {        
-        const data = await response.json();
-        console.log(data);
-
-        function card() {
-            let name = document.createElement('p');
-            let date = document.createElement('p');
-            let temperature = document.createElement('p');
-            let humidity = document.createElement('p');
-            let wind = document.createElement('p');
-            let indexUV = document.createElement('p');
-            name.innerHTML = data.name;
-            // date.innerHTML = data.date;
-            temperature.innerHTML = data.main.temp;
-            humidity.innerHTML = data.main.humidity;
-            wind.innerHTML = data.wind.speed;
-            indexUV.innerHTML = 
-
-            city.appendChild(name);
-            city.appendChild(temperature);
-            document.body.appendChild(city);            
-        };
-        card();
     })
 };
 
