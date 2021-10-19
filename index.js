b31dce249942cdca6cc002963024760d5075e7a9
Sirmaaalik <malikrharris@gmail.com>
2021-10-18 20:29:40 -0400

    Added another api for lat and long only. Fixing the second api

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // Title
 var title = document.createElement('div')
 let titleName = document.createElement('p');
-titleName.innerHTML = "Weather forecast";
+titleName.innerHTML = "Weather Dashboard";
 title.appendChild(titleName);
 document.body.appendChild(title);
 
-// Search for city section
+// Search for city section & events
 var search = document.createElement('div');
 let searchInput = document.createElement('input');
-// Events for search
-searchInput.addEventListener("keypress", event => {
-    if(event.keyCode === 13) {
-        event.preventDefault();
+searchInput.addEventListener("keypress", enter => {
+    if(enter.keyCode === 13) {
+        enter.preventDefault();
         weather();
     }
 });
@@ -21,34 +20,50 @@ buttonInput.addEventListener("click", mouse => {
     mouse.preventDefault();
     weather();
 })
-
-var weather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=miami&appid=734bbaa3706a832f46be371dd62d57a4").then((response) => response.json())
-    .then((responseJson) => {
-        console.log(responseJson);
-    })
-    
-    
-    // let display = function (repos, searchTerm) {
-    //         console.log(repos);
-    //         console.log(searchTerm);
-    //     };
-    //     response.json().then(function(data) {
-    //         display(data, cod)
-    //     });
-    // });
-}
-// buttonInput.addEventListener("click", function() {
-//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=miami&appid=734bbaa3706a832f46be371dd62d57a4`;
-// });
-
-// Append the search
 search.appendChild(searchInput);
 search.appendChild(buttonInput);
 document.body.appendChild(search);
 
-// City results
+// Weather info
 var city = document.createElement('div');
+var weather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=miami&appid=734bbaa3706a832f46be371dd62d57a4").then(
+        async placement => {
+            const place = await placement.json();
+            console.log(place);
+            let latitude = document.createElement('p');
+            let longitude = document.createElement('p');
+            latitude.innerHTML = place.coord.lat;
+            longitude.innerHTML = place.coord.lon;
+            console.log(latitude);
+            console.log(longitude);
+        },
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=734bbaa3706a832f46be371dd62d57a4`),
+        async response => {        
+        const data = await response.json();
+        console.log(data);
+
+        function card() {
+            let name = document.createElement('p');
+            let date = document.createElement('p');
+            let temperature = document.createElement('p');
+            let humidity = document.createElement('p');
+            let wind = document.createElement('p');
+            let indexUV = document.createElement('p');
+            name.innerHTML = data.name;
+            // date.innerHTML = data.date;
+            temperature.innerHTML = data.main.temp;
+            humidity.innerHTML = data.main.humidity;
+            wind.innerHTML = data.wind.speed;
+            indexUV.innerHTML = 
+
+            city.appendChild(name);
+            city.appendChild(temperature);
+            document.body.appendChild(city);            
+        };
+        card();
+    })
+};
 
 // Forecast results
 var forecast = document.createElement('div');
